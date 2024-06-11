144cefae45feb7a07158c6fc4754286b5bfb6b1b
gaelWumba <gael.wumbabuatu@gmail.com>
2024-06-11 16:37:12 +0200

    Initial Commit

---
+++ echos/echosApp.swift
@@ -0,0 +1,17 @@
+//
+//  echosApp.swift
+//  echos
+//
+//  Created by gael on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct echosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
